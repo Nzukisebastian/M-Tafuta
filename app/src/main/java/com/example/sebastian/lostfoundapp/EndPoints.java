9126fe74c62b6975e784c7495a97d7045a97f964
Nzukisebastian <nzuki2030@gmail.com>
2018-08-27 14:17:52 +0300

    automatic firebase cloud messaging

---
+++ app/src/main/java/com/example/sebastian/lostfoundapp/EndPoints.java
@@ -0,0 +1,14 @@
+package com.example.sebastian.lostfoundapp;
+
+/**
+ * Created by Nzuki on 8/27/2018.
+ */
+public class EndPoints {
+
+
+        public static final String URL_REGISTER_DEVICE = "http://10.0.2.2/FcmExample/RegisterDevice.php";
+        public static final String URL_SEND_SINGLE_PUSH = "http://10.0.2.2/FcmExample/sendSinglePush.php";
+        public static final String URL_SEND_MULTIPLE_PUSH = "http://10.0.2.2/FcmExample/sendMultiplePush.php";
+        public static final String URL_FETCH_DEVICES = "http://10.0.2.2/FcmExample/GetRegisteredDevices.php";
+
+}
